fb9bf3a2914d12b5d60fe6a2b60aeee8612d710d
BuildTools <unconfigured@null.spigotmc.org>
2021-12-16 17:19:39 +0100

    add tp4

---
+++ tp4.php
@@ -0,0 +1,41 @@
+<!-- Developed by Raphael BIRON for ESGI ©️ 2021 -->
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+
+<head>
+    <!-- <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./assets/css/style.css"> -->
+    <title>TP4 Three.js</title>
+    <?php include('includes/headContent.php') ?>
+</head>
+
+<body>
+    <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+    <div class="overlay">
+        <div class="lds-ctnr">
+            <h2 class="title">Chargement...</h2>
+            <div class="lds-facebook">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <input type="button" class="btn" value="Play">
+        </div>
+    </div>
+
+    <div class="bottom"></div>
+
+    <script src="./assets/scripts/tp4.core.js" type="module"></script>
+</body>
+
+</html>
